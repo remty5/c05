ac523560c2c5ef14433aa98aea7fd2cfa0eb5a7c
rvandepu <rvandepu@student.42lehavre.fr>
2023-07-11 18:49:16 +0200

    ex05

---
+++ ex05/ft_sqrt.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvandepu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 18:19:19 by rvandepu          #+#    #+#             */
+/*   Updated: 2023/07/11 18:49:10 by rvandepu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_sqrt(int nb)
+{
+	int	n;
+
+	if (nb < 1)
+		return (0);
+	n = 1;
+	while (n * n != nb && n < nb / 2)
+		n++;
+	if (n * n == nb)
+		return (n);
+	else
+		return (0);
+}
+
+/*int	main(int argc, char *argv[])
+{
+	if (argc == 2)
+		printf("ft_sqrt(%s) = %d\n", argv[1], ft_sqrt(atoi(argv[1])));
+	else
+		printf("Usage: %s <number>\n", argv[0]);
+}*/
